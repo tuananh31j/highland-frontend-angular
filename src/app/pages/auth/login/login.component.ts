ed538eb0cf1e68c1218a59c8c104a147e26cd905
Nguyen Tuan Anh <tuananh31j@gmail.com>
2024-01-13 11:04:05 +0700

    feat: login

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,10 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+    FormBuilder,
+    FormGroup,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms';
+import { bootstrapApplication } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-login',
     standalone: true,
-    imports: [],
+    imports: [ReactiveFormsModule, CommonModule],
+    providers: [FormBuilder, FormGroup, Validators],
     templateUrl: './login.component.html',
     styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+    loginForm: FormGroup;
+
+    constructor(private formBuilder: FormBuilder) {
+        this.loginForm = this.formBuilder.group({
+            email: ['', Validators.required],
+            pwd: ['', Validators.required],
+        });
+    }
+
+    onSubmit() {
+        if (!this.loginForm.valid) return console.log(this.loginForm.value);
+    }
+}
+bootstrapApplication(LoginComponent);
